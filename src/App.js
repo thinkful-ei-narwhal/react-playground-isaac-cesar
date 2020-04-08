bc4f0b1b563d5e0c53000ee580d083caed954828
Cesar Morales <cesarlenin2@live.com>
2020-04-08 09:34:04 -0700

    drill 2 complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import HelloWorld from './state-drills/HelloWorld';
+import Bomb from './state-drills/Bomb';
+
 
 function App() {
 	return (
 		<div className='App'>
 			<HelloWorld />
+			<Bomb/>
 		</div>
 	);
 }
---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class Bomb extends React.Component {
+
+    state = {
+        count: 0   
+    }
+
+
+
+    componentDidMount(){
+        const callback = e =>{
+            this.setState({
+                count : this.state.count + 1
+            })
+        }
+        this.interval= setInterval(callback, 1000)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+    renderCount(){
+        const {count}= this.state
+        if(count %2 ===0){
+            return <p>tick</p>
+        }else if (count <8){
+            return <p>tock</p>
+        }else{
+            clearInterval(this.interval)
+            return <p>bomb</p>
+        }
+
+    }
+
+
+
+    render(){
+        console.log(this.state.count);
+        return(
+        <div>
+            {this.renderCount()}
+        </div>
+        )
+    }
+}
+
+export default Bomb;
