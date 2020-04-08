932181dea5d6e2a0f6a2eb45c3912d5855ef0291
Isaac Elesia <isaacelesia@gmail.com>
2020-04-08 13:37:17 -0500

    drill 4 comleted

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,26 +1,34 @@
 import React from 'react';
 
 class Accordion extends React.Component {
+	state = {
+		content: '',
+	};
 
-    handleDisplay(){
-        return this.props.sections.map((e, index)=>{
-            return <li key= {index} ><button onClick= {<p>{e.content}</p>}>{e.title}</button></li>
-        })
-    }
+	handleDisplay() {
+		return this.props.sections.map((e, index) => {
+			return (
+				<li key={index}>
+					<button
+						onClick={() => {
+							return this.setState({ content: e.content });
+						}}
+					>
+						{e.title}
+					</button>
+				</li>
+			);
+		});
+	}
 
-    // handleClick(e){
-    //     return <p>{e.content}</p>
-    // }
-
-    render(){
-        return(
-            <div>
-                <ul>
-                    {this.handleDisplay()} 
-                </ul>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div>
+				<ul>{this.handleDisplay()}</ul>
+				<p>{this.state.content}</p>
+			</div>
+		);
+	}
 }
 
 export default Accordion;
