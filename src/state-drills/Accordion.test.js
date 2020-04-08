bc893b32b5d31aaa398d06a357a15e68e4959a13
Cesar Morales <cesarlenin2@live.com>
2020-04-08 12:44:10 -0700

    test 3 finished

--- src/state-drills/Accordion.test.js
+++ src/state-drills/Accordion.test.js
@@ -3,10 +3,30 @@ import renderer from 'react-test-renderer'
 import Accordion from'./Accordion'
 
 describe('accordion',()=>{
-
+    const sections = [
+		{
+		  title: 'Section 1',
+		  content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+		},
+		{
+		  title: 'Section 2',
+		  content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+		},
+		{
+		  title: 'Section 3',
+		  content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+		},
+	  ]
     it('renders empty ul when section props null',() =>{
         expect(
             renderer.create(<Accordion />).toJSON()
             ).toMatchSnapshot()
     })
+
+    it('renders no sections as active by default',() =>{
+        expect(
+            renderer.create(<Accordion sections = {sections}/>).toJSON()
+            ).toMatchSnapshot()
+    })
+
 })
